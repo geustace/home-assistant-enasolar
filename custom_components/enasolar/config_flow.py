34af97bf31a119cc3c87d1eddacef25dbffe0a66
Glen Eustace <glen@eustace.nz>
2024-06-01 15:42:12 +1200

    missing declaration

--- custom_components/enasolar/config_flow.py
+++ custom_components/enasolar/config_flow.py
@@ -11,6 +11,7 @@ import pyenasolar
 import voluptuous as vol
 
 from homeassistant import config_entries
+from homeassistant.core import callback
 from homeassistant.data_entry_flow import FlowResult
 import homeassistant.helpers.config_validation as cv
 
